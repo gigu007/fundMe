a0debc7d3d251cce21243a5faf649c4f65260ce6
gigu007 <gideonukaegbu007@gmail.com>
2022-06-30 13:34:50 +0100

    first commit

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,48 @@
+const {network}=require("hardhat");
+const{DECIMALS,INITIAL_ANSWER}=require("../helper-hardhart-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    // If we are on a local develohelppment network, we need to deploy mocks!
+    if (chainId == 31337) {
+        log("Local network detected! Deploying mocks...")
+        await deploy("MockV3Aggregator", {
+            contract: "MockV3Aggregator",
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_ANSWER],
+        })
+        log("Mocks Deployed!")
+        log("------------------------------------------------")
+        log(
+            "You are deploying to a local network, you'll need a local network running to interact"
+        )
+        log(
+            "Please run `npx hardhat console` to interact with the deployed smart contracts!"
+        )
+        log("------------------------------------------------")
+    }
+}
+module.exports.tags = ["all", "mocks"]
+
+// module.exports=async({getNamedAccounts,deployments})=>{
+//     const {deploy,log}=deployments
+//     const{deployer}=await getNamedAccounts()
+//     const chainId=network.config.chainId
+//     if(developmentChains.includes(network.name)){
+//         log("Local network detected! Deploying mocks... ")
+//         await deploy("MockV3Aggregator",{
+//             contract:"MockV3Aggregator",
+//             from:deployer,
+//             log:true,
+//             args:[DECIMALS,INITIAL_ANSWER],
+            
+
+//         })
+//         log("Mocks deployed")
+//         log("--------------------------")
+//     }
+// }
+// module.exports.tags=["all","mocks"]
---
+++ hardhat.config.js
@@ -0,0 +1,128 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const KOVAN_RPC_URL =
+    process.env.KOVAN_RPC_URL ||
+    "https://mainnet.infura.io/v3/fdb9960fee154d058a8b211b3cb550cd"
+// const RINKEBY_RPC_URL =
+//     process.env.RINKEBY_RPC_URL ||
+//     "https://mainnet.infura.io/v3/fdb9960fee154d058a8b211b3cb550cd"
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+module.exports = {
+    // defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        kovan: {
+            url: KOVAN_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 42,
+            blockConfirmations: 6,
+        },
+        rinkeby: {
+            url: "https://rinkeby.infura.io/v3/92695d013ef64f9abdbcdfb1f7b951e8",
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
+}
+
+
+
+
+
+// require("dotenv").config();
+
+// require("@nomiclabs/hardhat-etherscan");
+// require("@nomiclabs/hardhat-waffle");
+// require("hardhat-gas-reporter");
+// require("solidity-coverage");
+// require("hardhat-deploy")
+
+
+
+// // You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+// /**
+//  * @type import('hardhat/config').HardhatUserConfig
+//  */
+// const RINKEBY_RPC_URL=process.env.RINKEBY_RPC_URL
+// const PRIVATE_KEY=process.env.PRIVATE_KEY
+// const COINMARKET_API_KEY=process.env.COINMARKET_API_KEY
+// const ETHERSCAN_API_KEY=process.env.ETHERSCAN_API_KEY
+// module.exports = {
+//   // solidity: "0.8.8",
+//   solidity:{
+//     compilers:[
+//       {version:"0.8.8"},{version:"0.6.6"},
+//     ]
+//   },
+//   defaultNetwork:"hardhat",
+//   networks: {
+//     url:RINKEBY_RPC_URL,
+//     accounts:[PRIVATE_KEY],
+//     chainId:4,
+//     blockConfirmations:6,
+//   },
+//   gasReporter: {
+//     enabled: false,
+//     currency: "USD",
+//     outputFile:"gas-report.txt",
+//     noColors:true,
+//     coinmarketcap:COINMARKET_API_KEY,
+//     token:"MATIC",
+//   },
+//   etherscan: {
+//     apiKey: ETHERSCAN_API_KEY,
+//   },
+//   namedAccounts:{
+//     deployer:{
+//       default:0,
+
+//     }
+//   }
+// };
